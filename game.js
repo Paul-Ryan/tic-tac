4b44cb326e0701a8e32a7f08c51147179d30f24e
Paul Ryan <paul.ryan@quantcast.com>
2022-01-24 10:22:44 -0800

    Setup game board.

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
-const GameBoard = {};
+const Game = (() => {
+  // module pattern - invoked function, single instance of module
+  const GameBoard = (() => {
+    const board = [null, "X", null, "O", null, null, null, null, null];
+
+    getBoard = () => board;
+
+    return { getBoard };
+  })();
+
+  // factory function - returns an object
+  const playerFactory = (name, symbol) => {
+    getName = () => name;
+    getSymbol = () => symbol;
+
+    return { getName, getSymbol };
+  };
+
+  const controlFlow = (() => {
+    const currentPlayer = 1;
+    const changePlayer = () => {
+      currentPlayer = currentPlayer === 1 ? 2 : 1;
+    };
+
+    return { currentPlayer, changePlayer };
+  })();
+
+  const selectSquare = () => {};
+
+  return { GameBoard, playerFactory };
+})();
