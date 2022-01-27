ea1602a636495e095ec7cd5ab91afa372ab3d1e9
Paul Ryan <paul.ryan@quantcast.com>
2022-01-26 22:09:15 -0800

    Setup initial game state and board.

--- game.js
+++ game.js
@@ -1,31 +1,41 @@
-const Game = (() => {
-  // module pattern - invoked function, single instance of module
-  const GameBoard = (() => {
-    const board = [null, "X", null, "O", null, null, null, null, null];
+// module pattern - invoked function, single instance of module
+const GameBoard = (() => {
+  const board = new Array(null, null, null, null, null, null, null, null, null);
+  const getBoard = () => board;
+  const updateSpace = (event) => {
+    console.log(event);
 
-    getBoard = () => board;
-
-    return { getBoard };
-  })();
-
-  // factory function - returns an object
-  const playerFactory = (name, symbol) => {
-    getName = () => name;
-    getSymbol = () => symbol;
-
-    return { getName, getSymbol };
+    gameController.changePlayer();
   };
 
-  const controlFlow = (() => {
-    const currentPlayer = 1;
-    const changePlayer = () => {
-      currentPlayer = currentPlayer === 1 ? 2 : 1;
-    };
+  return { getBoard, updateSpace };
+})();
 
-    return { currentPlayer, changePlayer };
+// factory function - returns an object
+const playerFactory = (name, symbol) => {
+  getName = () => name;
+  getSymbol = () => symbol;
+
+  return { getName, getSymbol };
+};
+
+const gameController = (() => {
+  let currentPlayer = 1;
+  const changePlayer = () => {
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+  };
+
+  return { currentPlayer, changePlayer };
+})();
+
+const displayController = (() => {
+  const _init = (() => {
+    const squares = document.querySelectorAll(".square");
+    const board = GameBoard.getBoard();
+    squares.forEach((square) => {
+      const idx = square.dataset.id;
+      square.innerHTML = board[idx];
+      square.addEventListener("click", GameBoard.updateSpace);
+    });
   })();
-
-  const selectSquare = () => {};
-
-  return { GameBoard, playerFactory };
 })();
